a08965a187fda76fc29858a729c1c664348516a1
Duc13 <ducldts01610@gmail.com>
2026-01-22 08:23:59 +0700

    Hoàn thành full lab 4

--- Lab4/src/App.vue
+++ Lab4/src/App.vue
@@ -1,21 +1,16 @@
 <script setup>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import HelloWorld from './components/HelloWorld.vue';
+import Bai3 from './components/Bai3.vue';
+import Bai4 from './components/Bai4.vue';
 </script>
-<template>
-<Hello></Hello>
-</template>
 <style scoped></style>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <HelloWorld/>
+  <Bai3 /> -->
+  <Bai4/>
 </template>
 
 <style scoped>
@@ -25,9 +20,11 @@ import HelloWorld from './components/HelloWorld.vue';
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
