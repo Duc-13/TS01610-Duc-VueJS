653a7b60881eee38da22f15519958b2268ae2e0a
Duc13 <ducldts01610@gmail.com>
2026-01-28 14:12:45 +0700

    Xong bài 1 lab 6

---
+++ Lab6/src/components/Bai1.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="container">
+<h3>Nhập điểm số của bạn: </h3>
+<input type="number" v-model.number="score" />
+<p v-if="score < 1 || score > 10" style="color: red">Vui lòng nhập điểm từ 1 đến 10 !</p>
+<p v-else-if="score <= 3">Yếu</p>
+<p v-else-if="score <= 6">Trung bình</p>
+<p v-else-if="score <= 8">Khá</p>
+<p v-else>Giỏi</p>
+</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const score = ref('1');
+
+</script>
