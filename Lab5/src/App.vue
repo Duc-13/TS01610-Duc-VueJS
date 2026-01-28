8ed13d5e1952c15572f6631f4316f6008270e9bf
Duc13 <ducldts01610@gmail.com>
2026-01-28 13:51:45 +0700

    Hoàn thành bài 1 Lab 5

---
+++ Lab5/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+import Bai1 from './components/Bai1.vue';
+import Bai2 from './components/Bai2.vue';
+import Bai3 from './components/Bai3.vue';
+import PostList from './components/PostList.vue';
+import CreatePost from './components/CreatePost.vue';
+
+const posts = ref([])
+
+function addPost(post) {
+  posts.value.push(post)
+}
+</script>
+<template>
+  <!-- <Bai1/> -->
+  <!-- <Bai2/> -->
+  <!-- <Bai3/> -->
+  <div id="app">
+    <h1>Ứng dụng Blog nhỏ với Vue. js</h1>
+    <CreatePost @add-post="addPost" />
+    <PostList :posts="posts" />
+  </div>
+</template>
+
+<style scoped>
+#app {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
---
+++ Lab5/src/components/Bai1.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="p-5">
+        <p>{{ message }}</p>
+        <button class="btn btn-dark" @click="updateMessage">Thay đổi thông điệp</button>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+const message = ref('Thông điệp 1');
+const updateMessage = () => {
+    message.value = 'Thông điệp 2';
+};
+</script>
