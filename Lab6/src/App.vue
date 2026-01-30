988475c7034cd72af803a93dad9bd2242ae513ce
Duc13 <ducldts01610@gmail.com>
2026-01-30 21:40:48 +0700

    Hoàn thành bài 2

--- Lab6/src/App.vue
+++ Lab6/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Bai1 from './components/Bai1.vue';
+import Bai2 from './components/Bai2.vue';
 
 </script>
 
 <template>
-<Bai1/>
+<!-- <Bai1/> -->
+ <Bai2/>
 </template>
 
 <style scoped>
